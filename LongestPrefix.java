d15a0a93ffd2fbb5c890c0af42c40799c0a7261c
Shubham Sakhare <85404893+SHUBHAM7008@users.noreply.github.com>
2024-08-25 21:21:13 +0530

    Easy
    https://leetcode.com/problems/longest-common-prefix/submissions/1368028353/

--- LongestPrefix.java
+++ LongestPrefix.java
@@ -1,5 +1,26 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs,Comparator.comparing(s->s.length()));
+        int len = strs.length;
+        String prefix="";
+        for(int i=0;i<strs[0].length();i++){
+            boolean flag = false;
+            for(int j=1;j<len;j++){
+               if((strs[0].charAt(i))!=(strs[j].charAt(i))){
+                  return prefix;
+               }
+            }
+            prefix+=strs[0].charAt(i);
+
+        }
+        return prefix;
+    }
+}
+
+OR
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
         String prefix=strs[0];
         for(int i=1;i<strs.length;i++){
             String current=strs[i];
